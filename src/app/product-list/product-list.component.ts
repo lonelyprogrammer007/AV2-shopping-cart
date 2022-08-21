ba37b36a1ea8e6ec1498a7acf17e05a75b70f3f1
lugfigue <lugfigue@bancolombia.com.co>
2022-08-21 10:37:49 -0500

    finish

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,20 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { products } from '../products';
+import { Product, products } from "../products";
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"],
 })
 export class ProductListComponent {
-  products = products;
+  products = [...products];
 
   share() {
-    window.alert('The product has been shared!');
+    window.alert("The product has been shared!");
+  }
+
+  onNotify(e: any) {
+    console.log(e);
+    window.alert("You will be notified when the product goes on sale");
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
